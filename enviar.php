f3230829e47a5c7601dff6199905341e402e57a8
enfermerospilos <docencia2379@gmail.com>
2025-04-29 11:20:28 -0500

    Add files via upload
    initial commit

---
+++ enviar.php
@@ -0,0 +1,10 @@
+﻿<?php
+	$destino="docencia2379@gmail.com"
+	$nombre=$_post["Nombre"];
+	$Correo=$_post["Correo"];
+	$Telefono=$_post["Telefono"];
+	$mensaje=$_post["mensaje"];
+	$contenido="Nombre: " . $nombre . "\nCorreo:" . $correo . "\Telefono: " . $telefono . "\nmensaje: " . $mensaje;
+	mail($destino, "docencia2379@gmail.com");
+	header("Location:gracias.html");
+?>
